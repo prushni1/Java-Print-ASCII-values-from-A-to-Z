cf50ae9884d2cb4d613f4b234c6c86ac350490c9
prushni1 <jprushni@gmail.com>
2016-08-26 20:18:17 +0530

    Add files via upload

---
+++ DemoASCII.java
@@ -0,0 +1,16 @@
+
+public class DemoASCII {
+
+	//Write a program to print the characters corresponding to ASCII codes from 65 to 90.
+
+	 
+	
+	
+	public static void main(String args[])
+	{
+		for (int i = 65; i <= 90; i++) {
+		       //upper case A-Z
+		        System.out.println(i+" -  "+(char)i);
+		    }
+	}
+}
